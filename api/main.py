25243455e5b452841f36ee5ccc85861bc09a8cd9
Margo <vitarita095788@gmail.com>
2025-02-27 23:59:14 +0200

    fix deployment

--- api/main.py
+++ api/main.py
@@ -19,6 +19,7 @@ flask_app.register_all_blueprints(
 with flask_app.app_context:
     db.create_all()
 
+app = flask_app.app
 
 if __name__ == "__main__":
     flask_app.run(debug=FlaskAppConfig.DEBUG, port=DefaultConfig.PORT)

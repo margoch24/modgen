6056f40e065c1312ca8295ad3261fbedd8fcdc34
Margo <vitarita095788@gmail.com>
2025-03-01 12:59:02 +0200

    add logs

--- api/helpers/cronjobs.py
+++ api/helpers/cronjobs.py
@@ -6,11 +6,31 @@ from api.constants import Environment
 from config import DefaultConfig
 
 
+def list_directory_structure(start_path, indent_level=0):
+    try:
+        # List all entries in the given directory
+        entries = os.listdir(start_path)
+        # Indent for better readability
+        indent = " " * (indent_level * 4)
+
+        for entry in entries:
+            full_path = os.path.join(start_path, entry)
+            print(f"{indent}- {entry}")  # Print the entry name
+            if os.path.isdir(full_path):  # Check if it's a directory
+                # Recursively list the directory contents
+                list_directory_structure(full_path, indent_level + 1)
+    except Exception as e:
+        print(f"Error accessing {start_path}: {e}")
+
+
 def delete_files():
     print("started cronjob")
     current_dir = os.path.dirname(os.path.abspath(__file__))
     uploads_dir = os.path.join(current_dir, "../../uploads/")
     print(current_dir, uploads_dir)
+
+    print(os.path.exists(uploads_dir))
+    list_directory_structure(os.path.join(current_dir, "../../"))
 
     if not os.path.exists(uploads_dir):
         return
